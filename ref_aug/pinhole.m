70b79aaebfa6fa4dde8d57fd6ff7ffadc172b4c9
lkyahpu <44027069+lkyahpu@users.noreply.github.com>
2020-09-13 15:29:50 +0800

    Update pinhole.m

--- ref_aug/pinhole.m
+++ ref_aug/pinhole.m
@@ -2,7 +2,7 @@ clear vars;
 LF=zeros(512,512,9,9,3);
 
 
-imgPath='G:/LKY/epinet-master/hci_dataset/stratified/stripes/';
+imgPath='hci_dataset/training/boxes/';
 num=1;
 for i=1:9
   for j=1:9  
@@ -14,5 +14,5 @@ end
 LF_pinhole=reshape(permute(LF,[3 1 4 2 5]),[9*512 9*512 3]);
 %LF_pinhole=reshape(permute(LF,[1 3 2 4 5]),[9*512 9*512 3]);
 
-imwrite(LF_pinhole/255,'G:/LKY/淚적/code_matlab/stratified/stripes/stripes_all.png');
+imwrite(LF_pinhole/255,'boxes/boxes_all.png');
 
